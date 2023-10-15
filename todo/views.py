dd70842311c45bc5f2ebe7bea42bf66629ccbcf8
Silvio Schriefl <125505936+SilvioSchriefl@users.noreply.github.com>
2023-10-15 15:56:43 +0200

    update

--- todo/views.py
+++ todo/views.py
@@ -9,4 +9,4 @@ class TodoViewSet(viewsets.ModelViewSet):
     
     queryset = Todo.objects.all().order_by('created_at')
     serializer_class = TodoSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
