3f3f2254dc3f55d8d651747a704c9aadf7ac08ca
Silvio Schriefl <125505936+SilvioSchriefl@users.noreply.github.com>
2023-10-19 23:57:26 +0200

    update

--- todo/serializers.py
+++ todo/serializers.py
@@ -7,7 +7,7 @@ from .models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
     
-    author = serializers.CharField(source='author.email', read_only=True)
+    author = serializers.CharField(source='author.username', read_only=True)
     class Meta:
         model = Todo
         fields = [ 'pk','note', 'title', 'created_at', 'author', 'time_passed', 'checked']
--- todo/views.py
+++ todo/views.py
@@ -52,8 +52,34 @@ class TodoItemView(APIView):
         try:
             todo = Todo.objects.get(pk=pk)
             todo.delete()
-            todos = Todo.objects.all()
-            serializer = TodoSerializer(todos, many=True,)
-            return Response( serializer.data)
+            return Response(status=status.HTTP_204_NO_CONTENT)
         except Todo.DoesNotExist:
             raise NotFound(detail="Todo not found", code=status.HTTP_404_NOT_FOUND)
+        
+    def put(self, request, format=None):
+       
+        todo_id = request.data.get('pk')  
+        try:
+            todo = Todo.objects.get(pk=todo_id)
+        except Todo.DoesNotExist:
+            return Response({'error': 'Todo nicht gefunden'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    def patch(self, request, format=None):
+        try:
+            todo = Todo.objects.get(pk=request.data.get('pk')  )
+        except Todo.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = TodoSerializer(todo, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
